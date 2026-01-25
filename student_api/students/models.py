f6bd975d87b71585ad4b6c5b0014464dc5db596d
Nitesh Meena <neeteshmaran0123@gmail.com>
2026-01-25 21:02:21 +0530

    added new column college in table

--- student_api/students/models.py
+++ student_api/students/models.py
@@ -5,6 +5,6 @@ class Student(models.Model):
     email = models.EmailField(unique=True)
     age = models.IntegerField()
     course = models.CharField(max_length=100)
-
+    college=models.CharField(max_length=20,default="UIT")
     def __str__(self):
         return self.name
