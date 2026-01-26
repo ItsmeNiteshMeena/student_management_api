7e0ba67754d3cf8ba9707c65adb6f48f4a6e7cfa
Nitesh Meena <neeteshmaran0123@gmail.com>
2026-01-26 22:09:18 +0530

    updation in choices

--- student_api/students/models.py
+++ student_api/students/models.py
@@ -9,5 +9,6 @@ class Student(models.Model):
     city=models.CharField(max_length=20,null=True)
     mobile=models.IntegerField(max_length=10,null=True)
     address=models.CharField(max_length=20,null=True)
+    skills=models.TextField(max_length=30,null=True)
     def __str__(self):
         return self.name
